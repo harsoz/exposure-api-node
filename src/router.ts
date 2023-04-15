240d912be04da8877da6a8753ec64e4bd15b7a5f
harsoz <66538679+harsoz@users.noreply.github.com>
2023-04-15 14:07:20 -0600

    init 2

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import dllRoutes from './api/v1/routes/dll.route'
 
 export default function router(app: any): any {
+    app.get('/', (req, res) => {
+        res.send('Hello, World!'); // Send a response back to the client
+    });
     app.use('/api/v1/dll', dllRoutes);
     return app;
 }
